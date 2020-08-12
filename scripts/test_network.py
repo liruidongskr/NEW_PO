f76cb252c08586c573eaad3eb9a2ed859de9c7c0
liruidongskr <53814567+liruidongskr@users.noreply.github.com>
2020-08-12 13:36:32 +0800

    增加了一个4G方法

--- scripts/test_network.py
+++ scripts/test_network.py
@@ -18,3 +18,8 @@ class TestNetwork:
         self.network_page.click_move()
         self.network_page.click_typeNet()
         self.network_page.click_3G()
+
+    def test_neteork_4g(self):
+        self.network_page.click_move()
+        self.network_page.click_typeNet()
+        self.network_page.click_3G()
